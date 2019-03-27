20d14dec38aa8ced2a7a1a90096a58142fc265df
onlymyprojects <37711358+onlymyprojects@users.noreply.github.com>
2019-03-27 23:40:26 +0300

    Add files via upload

---
+++ iskragraphics2/js/scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		scrollingSpeed: 1000,
+		scrollBar: false,
+	});
+});
