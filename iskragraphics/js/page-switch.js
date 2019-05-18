64cc00a1bc2eb1ac79ad2f1cbaaba8733c185deb
onlymyprojects <37711358+onlymyprojects@users.noreply.github.com>
2019-05-18 23:36:39 +0300

    Add files via upload

--- iskragraphics/js/page-switch.js
+++ iskragraphics/js/page-switch.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
-	$('.b1').click(function(){
-		$('.b2, .b3, .b4, .b5, .b6, .b7, .b8').css({'opacity': '0', 'transition': '0.5s'});
-		$('.bb1').css({'padding-bottom': '20px', 'margin-top': '-20px'});
+	$('.i1').click(function(){
+		$('.i2, .i3, .i4, .i5, .i6, .i7, .i8').removeClass('visibility').removeClass('visibility2').animate({'opacity': '0'}, 150);
+		$('.i1').css({'margin-top': '-20px', 'box-shadow': '0 25px 25px -30px black'});
 		setTimeout(function(){
-			$('.b1').css({'opacity': '0', 'transition': '0.5s'});
-	    }, 500);
-		setTimeout(function(){
-			$(location).attr('href', 'portfolio-block1.html');
-	    }, 1000);
-    });
-    $('.b2').click(function(){
-		$('.b1, .b3, .b4, .b5, .b6, .b7, .b8').css({'opacity': '0', 'transition': '0.5s'});
-		$('.bb2').css({'padding-bottom': '20px', 'margin-top': '-20px'});
-		setTimeout(function(){
-			$('.b2').css({'opacity': '0', 'transition': '0.5s'});
+			$('.i1').removeClass('visibility').animate({'opacity': '0'}, 150);
 	    }, 500);
 		setTimeout(function(){
 			$(location).attr('href', 'portfolio-block1.html');
