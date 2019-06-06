72cb2b2df415fee7406785f8aac2467d805320ab
onlymyprojects <37711358+onlymyprojects@users.noreply.github.com>
2019-06-06 23:35:18 +0300

    Add files via upload

--- iskragraphics/js/page-switch.js
+++ iskragraphics/js/page-switch.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
 			$(location).attr('href', 'portfolio-block1.html');
 	    }, 1000);
     });
+
+
+		$('.i2').click(function(){
+			$('.i1, .i3, .i4, .i5, .i6, .i7, .i8').removeClass('visibility').removeClass('visibility2').animate({'opacity': '0'}, 150);
+			$('.i2').css({'margin-top': '-20px', 'box-shadow': '0 25px 25px -30px black'});
+			setTimeout(function(){
+				$('.i2').removeClass('visibility').animate({'opacity': '0'}, 150);
+		    }, 500);
+			setTimeout(function(){
+				$(location).attr('href', 'portfolio-block2.html');
+		    }, 1000);
+	    });
 });
