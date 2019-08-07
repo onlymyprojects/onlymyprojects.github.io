5e209aeb33386128747f090444ab129d4934039c
onlymyprojects <37711358+onlymyprojects@users.noreply.github.com>
2019-08-07 13:04:13 +0300

    Add files via upload

---
+++ iskragraphics/mail/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		console.log('lolelo');
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
