90ecd34d4c70a5201ed393002b752c7d6355f949
Bogdan <johnfeed500@gmail.com>
2020-05-26 15:19:14 +0300

    delete

--- iskragraphics/js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: true,
-		scrollingSpeed: 1000,
-		scrollBar: false,
-	});
-});
